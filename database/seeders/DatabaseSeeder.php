953032b018a7ad67b13adc9fb94853b63a11f378
Aliaksey Domashevich <aleksey.domashevich@gmail.com>
2024-05-03 15:14:31 +0300

    start

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
 use App\Models\Attr;
 use App\Models\Categories;
 use App\Models\Category;
 use App\Models\Color;
 use App\Models\Manufacture;
 use App\Models\Product;
+use App\Models\Question;
 use App\Models\Size;
+use App\Models\Track;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -34,6 +37,31 @@ class DatabaseSeeder extends Seeder
         Attr::factory(10)->create();
         User::factory(50)->create();
 
+        Track::truncate();
+        $this->addTracks();
+
+        $tracks = Track::all();
+        Question::truncate();
+        Answer::truncate();
+
+        $questionAndAnswers = $this->getData();
+        foreach ($tracks as $track) {
+            $questionAndAnswers->where('track', $track->id)->each(function ($question) {
+                $createdQuestion = Question::create([
+                    'text' => $question['question'],
+                    'track_id' => $question['track'],
+                    'points' => $question['points'],
+                ]);
+
+                collect($question['answers'])->each(function ($answer) use ($createdQuestion) {
+                    Answer::create([
+                        'question_id' => $createdQuestion->id,
+                        'text' => $answer['text'],
+                        'correct_one' => $answer['correct_one'],
+                    ]);
+                });
+            });
+        }
 
 
 
